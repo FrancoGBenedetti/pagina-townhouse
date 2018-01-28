c54d078919a575ad1e5c9e63f816c8cc21039ba6
rgalazb <galaz.becerra@gmail.com>
2018-01-28 14:25:03 -0300

    pagina proyectos agregada

--- script.js
+++ script.js
@@ -18,3 +18,114 @@ $('.count').each(function () {
         }
     });
 });
+
+let grafico1 = document.getElementById('grafico1').getContext('2d');
+let Chart1 = new Chart(grafico1, {
+      type: 'pie',
+      data: {
+        labels:['Left', 'Current'],
+        datasets:[{
+          label: 'Puertas Vendidas',
+          data: [
+            60,
+            40
+          ],
+          backgroundColor:[
+            '#9e9e9e',
+            '#e0e0e0 '
+          ]
+        }]
+      },
+      options: {
+        legend: {
+            display: false
+         },
+         tooltips: {
+            enabled: false
+         }
+      }
+    });
+
+
+
+let grafico2 = document.getElementById('grafico2').getContext('2d');
+let Chart2 = new Chart(grafico2, {
+      type: 'pie',
+      data: {
+        labels:['Left', 'Current'],
+        datasets:[{
+          label: 'Puertas Vendidas',
+          data: [
+            80,
+            20
+          ],
+          backgroundColor:[
+            '#9e9e9e',
+            '#e0e0e0 '
+          ]
+        }]
+      },
+      options: {
+        legend: {
+            display: false
+         },
+         tooltips: {
+            enabled: false
+         }
+      }
+    });
+
+
+let grafico3 = document.getElementById('grafico3').getContext('2d');
+let Chart3 = new Chart(grafico3, {
+      type: 'pie',
+      data: {
+        labels:['Left', 'Current'],
+        datasets:[{
+          label: 'Puertas Vendidas',
+          data: [
+            55,
+            45
+          ],
+          backgroundColor:[
+            '#9e9e9e',
+            '#e0e0e0 '
+          ]
+        }]
+      },
+      options: {
+        legend: {
+            display: false
+         },
+         tooltips: {
+            enabled: false
+         }
+      }
+    });
+
+let grafico4 = document.getElementById('grafico4').getContext('2d');
+let Chart4 = new Chart(grafico4, {
+      type: 'pie',
+      data: {
+        labels:['Left', 'Current'],
+        datasets:[{
+          label: 'Puertas Vendidas',
+          data: [
+            20,
+            80
+          ],
+          backgroundColor:[
+            '#9e9e9e',
+            '#e0e0e0 '
+          ]
+        }]
+      },
+      options: {
+        legend: {
+            display: false
+         },
+         tooltips: {
+            enabled: false
+         }
+      }
+    });
